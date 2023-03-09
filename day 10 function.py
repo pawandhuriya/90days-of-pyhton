c0ad3ba73169c9753753942af3d4bd66aaf5ddd7
pawandhuriya <pawandhuirya.outlook.com>
2023-03-09 11:58:21 +0530

    day 10

---
+++ day 10 function.py
@@ -0,0 +1,9 @@
+# function with output
+#  def my_fucntion()--
+# def format_name():
+#     f_name=input("Enter the first name ")
+#     l_name=input("Enter the last name ")
+#     print("welcome",f_name,l_name)
+
+# format_name()
+
