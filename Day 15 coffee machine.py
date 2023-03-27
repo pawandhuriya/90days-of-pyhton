b7791d14905cd5186e56b0c752d75563f5760fb1
pawandhuriya <pawandhuirya.outlook.com>
2023-03-27 12:43:38 +0530

    day 15 excersize process

--- Day 15 coffee machine.py
+++ Day 15 coffee machine.py
@@ -1,1 +1,49 @@
-#lets build
+# lets build coffe machine
+MENU = {
+    "espresso": {
+        "ingredients": {
+            "water": 50,
+            "coffee": 18,
+        },
+        "cost": 1.5,
+    },
+    "latte": {
+        "ingredients": {
+            "water": 200,
+            "milk": 150,
+            "coffee": 24,
+        },
+        "cost": 2.5,
+    },
+    "cappuccino": {
+        "ingredients": {
+            "water": 250,
+            "milk": 100,
+            "coffee": 24,
+        },
+        "cost": 3.0,
+    }
+}
+
+resources = {
+    "water": 300,
+    "milk": 200,
+    "coffee": 100,
+}
+profit = 0
+# step 1 user for asking "what would you Like"?(espresso/latte/cappuccino))
+is_on = True
+while is_on:
+    choice = input(f" What would you like? (espresso/latte/cappuccino): ")
+    if choice == "off":
+        is_on = False
+    elif choice == "report":
+        print(f"Water: {resources['water']}ml")
+        print(f"Milk: {resources['milk']}ml")
+        print(f"Coffee: {resources['coffee']}g")
+        print(f"Money: ${profit}")
+
+# print(choice)
+
+
+# check the resources sufficent
