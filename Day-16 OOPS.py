dd09998292190b0c3799db32d4f8d5ebe3995e83
pawandhuriya <pawandhuirya.outlook.com>
2023-03-28 16:01:45 +0530

    day 16 PreetyTable new function packing

--- Day-16 OOPS.py
+++ Day-16 OOPS.py
@@ -1,12 +1,21 @@
-from turtle import Turtle, Screen
+# from turtle import Turtle, Screen
+#
+# timmy = Turtle()
+# print(timmy)
+# timmy.shape("turtle")
+# timmy.color("red")
+# timmy.forward(100)
+#
+#
+# my_screen = Screen()
+# print(my_screen.canvheight)
+# my_screen.exitonclick()
 
-timmy = Turtle()
-print(timmy)
-timmy.shape("turtle")
-timmy.color("red")
-timmy.forward(100)
+# ---------------python package---------------------
+from prettytable import PrettyTable
 
+table = PrettyTable()
+table.add_column("Pokemon name ",["Pikachu","squral","charmandl"])
+table.add_column("Type",["Electric","water","fire"])
 
-my_screen = Screen()
-print(my_screen.canvheight)
-my_screen.exitonclick()
+print(table)
